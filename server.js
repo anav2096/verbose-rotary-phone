28ee2c19689b16207c3bd1203e29da706c98ad2d
anav <anav2096@gmail.com>
2022-03-15 18:50:50 -0400

    test commit

--- server.js
+++ server.js
@@ -2,17 +2,17 @@ const express = require('express');
 const app = express();
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
-
+const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
 // Use apiRoutes
-app.use('/api/notes', apiRoutes);
+app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
   });
   
